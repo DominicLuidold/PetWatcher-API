30ac57c9d6a66422623616d3b378e1ea791377e3
Dominic Luidold <dominic.luidold@gmail.com>
2019-07-14 01:27:50 +0200

    Add base controller

---
+++ src/app/Controllers/BaseController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PetWatcher\Controllers;
+
+use Slim\Container;
+
+class BaseController {
+    /**
+     * @var \Slim\Container $container Instance of dependency container
+     */
+    protected $container;
+
+    /**
+     * @var \Illuminate\Database\Capsule\Manager $db Instance of database manager
+     */
+    protected $db;
+
+    /**
+     * @var \Monolog\Logger $logger Instance of logger
+     */
+    protected $logger;
+
+    /**
+     * @var \PetWatcher\Validation\Validator $validator Instance of validator
+     */
+    protected $validator;
+
+    /**
+     * Create a new base controller
+     *
+     * @param \Slim\Container $container
+     */
+    public function __construct(Container $container) {
+        $this->container = $container;
+        $this->db = $container['db'];
+        $this->logger = $container['logger'];
+        $this->validator = $container['validator'];
+    }
+}
